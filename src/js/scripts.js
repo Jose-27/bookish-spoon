0410ccd2b2a5be919d1a3503d6a66406596f37cb
joe <joe@joes-mbp.myfiosgateway.com>
2022-09-04 00:24:10 -0300

    test 10

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -3,7 +3,6 @@ function senderForm() {
   var length = document.forms.length;
   let user = {
     "blog": "maryCargoExpress",
-    "trackingId": "5e40d4d2-4081-4607-b8d5-02018287e45a",
     "senderType": "sender"
   }
   for(var i = 0; i < length; i++) {
@@ -24,7 +23,7 @@ function senderForm() {
     } else if(element.id === 'address1') {
       user['address1'] = element.value.trim();
     } else if(element.id === 'address2') {
-      user['address2'] = element.value.trim();
+      user['address2'] = elemenst.value.trim();
     } else if(element.id === 'country' && element.value !== '') {
       user['country'] = element.value.trim();
     } else if(element.id === 'state') {
@@ -41,7 +40,6 @@ function receiverForm() {
   var length = document.forms.length;
   let user = {
     "blog": "maryCargoExpress",
-    "trackingId": "5e40d4d2-4081-4607-b8d5-02018287e45a",
     "clientType": "receiver"
   }
   for(var i = 0; i < length; i++) {
@@ -74,34 +72,33 @@ function receiverForm() {
   return user;
 }
 
-
-function registrationFormData() {
-postData('/api/CreateSubscriber', senderForm()).then((data)=>{
-  let receiverObj = receiverForm();
-  receiverObj.senderRowKey = data.RowKey
-  postData('/api/CreateSubscriber', receiverObj).then((yy)=>{
-    document.getElementById("receiverForm").reset();
-    document.getElementById("senderForm").reset();
-    window.location = '/tables.html';  
-  });
-});
-}
-
 // Example POST method implementation:
 async function postData(url, data) {
-// Default options are marked with *
-const response = await fetch(url, {
-  method: 'POST', // *GET, POST, PUT, DELETE, etc.
-  mode: 'cors', // no-cors, *cors, same-origin
-  cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-  credentials: 'same-origin', // include, *same-origin, omit
-  headers: {
-    'Content-Type': 'application/json'
-    // 'Content-Type': 'application/x-www-form-urlencoded',
-  },
-  redirect: 'follow', // manual, *follow, error
-  referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-  body: JSON.stringify(data) // body data type must match "Content-Type" header
-});
-return response.json(); // parses JSON response into native JavaScript objects
+  // Default options are marked with *
+  const response = await fetch(url, {
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    mode: 'cors', // no-cors, *cors, same-origin
+    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: 'same-origin', // include, *same-origin, omit
+    headers: {
+      'Content-Type': 'application/json'
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: 'follow', // manual, *follow, error
+    referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    body: JSON.stringify(data) // body data type must match "Content-Type" header
+  });
+  return response.json(); // parses JSON response into native JavaScript objects
+}
+
+function registrationFormData() {
+  postData('/api/CreateSubscriber', senderForm()).then((data)=>{
+    let receiverObj = receiverForm();
+    receiverObj['senderRowKey'] = data.RowKey
+    postData('/api/CreateSubscriber', receiverObj).then(()=>{
+      document.getElementById("receiverForm").reset();
+      document.getElementById("senderForm").reset();
+      window.location = '/tables.html';  
+    });
+  });
 }
